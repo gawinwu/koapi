dcfa9bfea0c836a42add8c494325b18650ff3b60
gawinwu <gawinwu@163.com>
2019-11-19 04:57:34 +0800

    取消Book Model构造函数

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -30,8 +30,10 @@ router.get('/hot_list', async ctx => {
 
 router.get('/:id/detail', async ctx => {
     const v = await new PositiveIntegerValidator().validate(ctx)
-    const book = new Book(v.get('path.id'))
-    ctx.body = await book.detail()
+    // const book = new Book(v.get('path.id'))
+    // ctx.body = await book.detail()
+    const book = new Book()
+    ctx.body = await book.detail(v.get('path.id'))
 })
 
 router.get('/search', async ctx => {
